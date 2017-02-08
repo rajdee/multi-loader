5bac00d2ba4d171ef1b6e5750b3ec782fdb09b7f
Ruslan Abdullaev <skarlatin@mail.ru>
2017-02-08 14:45:52 +0300

    fixes for compatibility with webpack 2.x

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 var loader = require.resolve("./index.loader.js")
 module.exports = function(items) {
-	if(!Array.isArray(items))
-		items = Array.prototype.slice.call(arguments);
-	return loader + "?" + JSON.stringify(items).replace(/!/g, "\\u0021");
+  var itemsArray = Array.prototype.slice.call(arguments)
+      .map((item) => {
+        return item.reduce((sum, current, index, arr) => {
+          var delimiter = (index !== arr.length - 1) ? '!' : '';
+          var options = (current.options && typeof current.options === 'object')
+                        ? '?' + JSON.stringify(current.options)
+                        : '';
+          return sum + current.loader + options + delimiter;
+        }, '');
+  });
+
+  return loader + "?" + JSON.stringify(itemsArray).replace(/!/g, "\\u0021");
 };
--- index.loader.js
+++ index.loader.js
@@ -1,6 +1,5 @@
 module.exports = function() {};
 module.exports.pitch = function(request) {
-	this.cacheable();
 	var items = JSON.parse(this.query.substr(1));
 	items = items.map(function(item) {
 		return "require(" + JSON.stringify("-!" + item + "!" + request) + ");";
